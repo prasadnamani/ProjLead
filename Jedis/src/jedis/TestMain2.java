f66e4427677b7883e32cb2779adacd801dd742fa
Namani Prasad <prasadnamani789@gmail.com>
2023-03-21 13:07:35 +0530

    added more features

--- Jedis/src/jedis/TestMain2.java
+++ Jedis/src/jedis/TestMain2.java
@@ -5,6 +5,8 @@ public class TestMain2 {
 	public void display() {
 		
 		System.out.println("hello world from TestMain2.java file .......");
+		System.out.println("calling calculator.java ");
+		System.out.println("calling getInfo.java ");
 		
 	}
 	
